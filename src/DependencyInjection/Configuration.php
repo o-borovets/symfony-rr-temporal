c12ba800c7c766edce37b6e1c72be1bbd7ac9923
Oleksandr Borovets <oleksandr.borovets@gmail.com>
2022-11-05 18:44:21 +0200

    Declared the bundle config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,17 +6,31 @@ namespace RoadRunnerTemporalSymfony\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Temporal\Worker\WorkerFactoryInterface;
 
 class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('symfony_roadrunner_temporal');
+        $treeBuilder = new TreeBuilder('temporal_symfony');
         $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
-            ->scalarNode('address')->defaultValue('127.0.0.1:7233')
+            ->scalarNode('address')
+                ->defaultValue('127.0.0.1:7233')
+            ->end()
+            ->scalarNode('namespace')
+                ->defaultValue('default')
+            ->end()
+            ->scalarNode('defaultWorker')
+                ->defaultValue(WorkerFactoryInterface::DEFAULT_TASK_QUEUE)
+            ->end()
+            ->arrayNode('workers')
+                ->scalarPrototype()
+                    ->defaultValue(WorkerFactoryInterface::DEFAULT_TASK_QUEUE)
+                ->end()
+            ->end()
         ;
 
         return $treeBuilder;
